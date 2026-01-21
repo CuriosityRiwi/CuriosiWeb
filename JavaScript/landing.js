ab2757ce60df2aa9d58191f7b94a35518e4ceeeb
valeps <valepserna@gmail.com>
2026-01-21 09:56:34 -0500

    Actualización del front de la landing

--- JavaScript/landing.js
+++ JavaScript/landing.js
@@ -1,5 +1,4 @@
-// main.js
-
+// Main Landing
 const products = [
     { name: "AeroPro Strike G5", category: "RACKETS", price: 249.00, img: "./img/shoes/img-tenis-man (1).jpg" },
     { name: "SwiftCourt Elite X", category: "FOOTWEAR", price: 129.99, img: "./img/shoes/img-tenis-man (2).jpg" },
@@ -7,32 +6,31 @@ const products = [
     { name: "Championship Extra Duty", category: "BALLS", price: 14.00, img: "./img/shoes/img-tenis-woman (2).jpg" }
 ];
 
+const container = document.getElementById("list-shoes");
 
 //Renderiza la informacion que trae de el contenedor de lista-cards
-const renderProducts = () => {
-    const container = document.getElementById("list-shoes");
+const renderProducts = () => {    
     let html = "";
 
     products.forEach(p => {
-        html += `
+        container.innerHTML += `
         <div class="col-12 col-md-6 col-lg-3">
-            <div class="card h-100 border-0 shadow-sm rounded-4 position-relative">
-                <button class="btn btn-light position-absolute top-0 end-0 m-3 rounded-circle shadow-sm" style="width: 35px; height: 35px; padding: 0;">
+            <div class="card border rounded-2 position-relative">
+                <button class="btn btn-light position-absolute top-0 end-0 m-3 rounded-circle" style="width: 35px; height: 35px; padding: 0;" padding: 0;">
                     <i class="bi bi-heart"></i>
                 </button>
-                <img src="${p.img}" class="card-img-top rounded-top-4" alt="${p.name}" style="height: 200px; object-fit: contain;">
+                <img src="${p.img}" class="card-img-top" alt="${p.name}" object-fit: contain;">
                 <div class="card-body">
                     <small class="text-success fw-bold">${p.category}</small>
                     <h6 class="fw-bold mt-1">${p.name}</h6>
                     <div class="d-flex justify-content-between align-items-center mt-3">
-                        <span class="fw-bold fs-5">$${p.price.toFixed(2)}</span>
-                        <button class="btn btn-outline-dark btn-sm rounded-pill px-3">View Info <i class="bi bi-info-circle"></i></button>
+                        <span class="fw-bold fs-5">$${p.price}</span>
+                        <button class="btn btn-outline-dark btn-sm rounded-pill px-3">View Info </button>
                     </div>
                 </div>
             </div>
         </div>`;
     });
-    container.innerHTML = html;
 };
 
 // Función para WhatsApp
@@ -44,4 +42,12 @@ const abrirWpp = (coleccion) => {
 
 // Ejecutar al cargar
 renderProducts();
+
+
+
+
+
+
+
+
 
